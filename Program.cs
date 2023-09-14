5b1b720138250eb7efd56151d8ca2a6117793a81
Matthew Hanson <mhanson.nflk@gmail.com>
2023-09-14 04:53:01 +0100

    Minor Formatting Adjustment

--- Program.cs
+++ Program.cs
@@ -1,42 +1,42 @@
-﻿using System;
-using Microsoft.VisualBasic;
-using Terminal.Gui;
+﻿using Terminal.Gui;
 
-//Init Terminal UI
-Application.Init();
-//quick alias for top level UI
-var top = Application.Top;
-
-//Primary window
-var mainWindow = new Window("NetNote")
+internal class Program
 {
-    X = 0,
-    Y = 1,
+    private static void Main(string[] args)
+    {
+        //Initialize GUI
+        Application.Init();
+        //quick alias for top level UI
+        var top = Application.Top;
 
-    Width = Dim.Fill(),
-    Height = Dim.Fill()
-};
-//add primary window to element
-top.Add(mainWindow);
+        //Primary window
+        var mainWindow = new Window("NetNote")
+        {
+            X = 0,
+            Y = 1,
 
-var mainMenu = new MenuBar(new MenuBarItem[]{
-new MenuBarItem("_File",new MenuItem[]{
-    new MenuItem("_New","Creates new note", null),
-    new MenuItem("_Quit","Quits the Application",() => {if (Quit ()) top.Running = false;})
-})
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+        //add primary window to element
+        top.Add(mainWindow);
 
+        var mainMenu = new MenuBar(new MenuBarItem[]{
+            new MenuBarItem("_File",new MenuItem[]{
+            new MenuItem("_New","Creates new note", null),
+            new MenuItem("_Quit","Quits the Application",() => {if (Quit ()) top.Running = false;})
+            })
+        });
 
-});
-top.Add(mainMenu);
+        top.Add(mainMenu);
 
-
-
-// Quit prompt
-static bool Quit()
-{
-    var n = MessageBox.Query(50, 7, "Quit NetNote", "Are you sure you want to quit this app?", "Yes", "No");
-    return n == 0;
+        // Quit prompt
+        static bool Quit()
+        {
+            var n = MessageBox.Query(50, 7, "Quit NetNote", "Are you sure you want to quit this app?", "Yes", "No");
+            return n == 0;
+        }
+        //Execute GUI
+        Application.Run();
+    }
 }
-//Execute GUI
-Application.Run();
-
