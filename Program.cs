9cb1bf3202b45a184a47ff419d19ad0a07d5e80d
Matthew Hanson <mhanson.nflk@gmail.com>
2023-09-14 04:39:45 +0100

    Initial Terminal UI Work

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using Microsoft.VisualBasic;
+using Terminal.Gui;
 
-namespace NetNotes
+//Init Terminal UI
+Application.Init();
+//quick alias for top level UI
+var top = Application.Top;
+
+//Primary window
+var mainWindow = new Window("NetNote")
 {
-    class program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World")
-        }
-    }
+    X = 0,
+    Y = 1,
+
+    Width = Dim.Fill(),
+    Height = Dim.Fill()
+};
+//add primary window to element
+top.Add(mainWindow);
+
+var mainMenu = new MenuBar(new MenuBarItem[]{
+new MenuBarItem("_File",new MenuItem[]{
+    new MenuItem("_New","Creates new note", null),
+    new MenuItem("_Quit","Quits the Application",() => {if (Quit ()) top.Running = false;})
+})
+
+
+});
+top.Add(mainMenu);
+
+
+
+// Quit prompt
+static bool Quit()
+{
+    var n = MessageBox.Query(50, 7, "Quit NetNote", "Are you sure you want to quit this app?", "Yes", "No");
+    return n == 0;
 }
+//Execute GUI
+Application.Run();
+
