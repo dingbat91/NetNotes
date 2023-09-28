d07700a09f801662070a157305a1a879a864bee2
Matthew Hanson <mhanson.nflk@gmail.com>
2023-09-28 15:22:16 +0100

    Added Note GUID

--- Notes.cs
+++ Notes.cs
@@ -1,9 +1,10 @@
 ﻿namespace NetNotes;
-
+using System;
 
 //Note Class - used to manage a single Note
 public class Note
 {
+    public string NoteID { get; set; }
     public string Title { get; set; }
     public string Body { get; set; }
     public DateTime Date { get; set; }
@@ -11,6 +12,7 @@ public class Note
 
     public Note(string title, string body, string[]? tags = null)
     {
+        NoteID = Guid.NewGuid().ToString();
         Title = title;
         Body = body;
         Date = DateTime.Now;
