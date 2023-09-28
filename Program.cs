a3d08533720e6bf9e038ce53c8122b758f0027d2
Matthew Hanson <mhanson.nflk@gmail.com>
2023-09-28 15:23:08 +0100

    Namespace Adjustments, save Functionality.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
-﻿using NetNotes;
-using Terminal.Gui;
-
-internal class Program
+﻿using Terminal.Gui;
+namespace NetNotes
 {
-
-    //Note Array, should be passed as ref at most times to keep data consistency
-    internal static List<Note> notes = new List<Note>();
-
-    private static void Main(string[] args)
+    internal class Program
     {
-        //Set Notes
-        var testNote = new Note("Intro Note", "You have no notes, so this is a default note to display in the UI!");
-        var testnote2 = new Note("Second Note", "This is a second note to display in the UI!");
-        notes = new List<Note>();
-        notes.Add(testNote);
-        notes.Add(testnote2);
 
-        //Initialize UI
-        Application.Init();
-        NoteUI.Initialize(notes);
+        //Note Array, should be passed as ref at most times to keep data consistency
+        internal static List<Note> notes = new List<Note>();
+
+        private static void Main(string[] args)
+        {
+            //Set Notes
+            var testNote = new Note("Intro Note", "You have no notes, so this is a default note to display in the UI!");
+            var testnote2 = new Note("Second Note", "This is a second note to display in the UI!");
+            notes = new List<Note>();
+            notes.Add(testNote);
+            notes.Add(testnote2);
+
+            //Initialize UI
+            Application.Init();
+            NoteUI.Initialize(notes);
+        }
     }
 }
